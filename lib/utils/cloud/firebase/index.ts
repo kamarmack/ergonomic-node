dc772cdf458fa9b1445f8e58cd531644273e1218
Kamar <kamarmack.home@gmail.com>
2024-05-31 13:25:38 +0000

    Add getFirebaseAuth function

--- lib/utils/cloud/firebase/index.ts
+++ lib/utils/cloud/firebase/index.ts
@@ -1,4 +1,5 @@
 export * from 'ergonomic-node/lib/utils/cloud/firebase/getCloudStorageBucket.js';
 export * from 'ergonomic-node/lib/utils/cloud/firebase/getFirebaseAdminServiceAccount.js';
 export * from 'ergonomic-node/lib/utils/cloud/firebase/getFirebaseApp.js';
+export * from 'ergonomic-node/lib/utils/cloud/firebase/getFirebaseAuth.js';
 export * from 'ergonomic-node/lib/utils/cloud/firebase/getFirestoreDB.js';
