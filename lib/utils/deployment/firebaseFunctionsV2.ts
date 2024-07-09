854d1646f0999b477b1b897b8cf114e749f0f9a2
Kamar <kamarmack.home@gmail.com>
2024-07-09 09:49:42 +0000

    Remove the type cast from the firebae functions constant

--- lib/utils/deployment/firebaseFunctionsV2.ts
+++ lib/utils/deployment/firebaseFunctionsV2.ts
@@ -1,14 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
 import * as functionsV2 from 'firebase-functions/v2';
-// eslint-disable-next-line import/no-unresolved
-import { type setGlobalOptions } from 'firebase-functions/v2/options';
-import { https, logger } from 'firebase-functions';
 
-const functions = functionsV2 as unknown as {
-	https: typeof https;
-	logger: typeof logger;
-	setGlobalOptions: typeof setGlobalOptions;
-};
+const functions = functionsV2;
 functionsV2.setGlobalOptions({
 	maxInstances: 10,
 });
