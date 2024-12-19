f1280cc0fab7b16b1a83566911a122f9393bb2b2
Kamar <kamarmack.home@gmail.com>
2024-12-19 04:03:13 +0000

    Remove unused middleware: initializeResLocalsWithGeneralizedResponseFields

--- lib/utils/middleware/index.ts
+++ lib/utils/middleware/index.ts
@@ -1,3 +1,2 @@
-export * from 'ergonomic-node/lib/utils/middleware/initializeResLocalsWithGeneralizedResponseFields.js';
 export * from 'ergonomic-node/lib/utils/middleware/logExpressInvocation.js';
 export * from 'ergonomic-node/lib/utils/middleware/sendExpressResponse.js';
--- lib/utils/middleware/initializeResLocalsWithGeneralizedResponseFields.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-
-export const initializeResLocalsWithGeneralizedResponseFields =
-	() =>
-	(_: express.Request, res: express.Response, next: express.NextFunction) => {
-		res.locals = { ...res.locals, data: [], errors: [] };
-		return next();
-	};
